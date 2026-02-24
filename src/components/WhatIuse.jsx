6139e2a5f34bcfe5520c1ecdee0b05aff5769f4c
sinan-nm <sinannm1212@gmail.com>
2026-02-24 14:02:03 +0530

    second commit

--- src/components/WhatIuse.jsx
+++ src/components/WhatIuse.jsx
@@ -14,14 +14,14 @@ const WhatIuse = () => {
       id="whatIuse"
       className="scroll-mt-24 py-20 bg-black overflow-hidden"
     >
-      {/* Heading */}
+  
       <div className="border-[3px] border-white px-10 py-3 bg-black mb-16 w-fit mx-auto">
         <h2 className="text-xl font-bold uppercase tracking-[0.5em] text-white">
           What I Use
         </h2>
       </div>
 
-      {/* Horizontal Moving Container */}
+ 
       <div className="relative w-full overflow-hidden">
         <motion.div
           className="flex gap-16 w-max"
@@ -32,7 +32,7 @@ const WhatIuse = () => {
             ease: "linear"
           }}
         >
-          {/* Duplicate array for smooth infinite effect */}
+       
           {[...language, ...language].map((tech, index) => (
             <div key={index} className="flex flex-col items-center min-w-37.5">
               <div className="w-20 h-20 md:w-24 md:h-24 bg-gray-50 rounded-xl flex items-center justify-center mb-4 shadow-sm border border-gray-100">
